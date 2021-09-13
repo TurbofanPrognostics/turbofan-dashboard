0721c7b080d0ef045be76a515d3f1ff6e79d2cc5
Abirvab Deb <adeb@concertai.com>
2021-09-13 19:52:16 -0400

    adding a form so simulation starts after an engine # and model is selected then press "Start Simulation"

--- turbofan_dashboard/dashboard.py
+++ turbofan_dashboard/dashboard.py
@@ -14,6 +14,16 @@ from random import sample
 import json
 from typing import List
 
+from html_header import html
+
+
+
+# backend request params
+port = 80
+# host = 'turbo-fast-app-dev.us-west-2.elasticbeanstalk.com'
+# use this host when testing local FastAPI docker deployment from streamlit
+host = 'host.docker.internal' 
+url = f'http://{host}:{port}'
 
 @st.cache
 def load_train_data() -> pd.DataFrame:
@@ -26,41 +36,22 @@ def load_train_data() -> pd.DataFrame:
 
     return train_df
 
+@st.cache
+def get_models():
+    response = requests.get(f'{url}/models')
+    return response.json()['models']
 
-html_header="""
-<head>
-<title>Predictive Maintenance</title>
-<meta charset="utf-8">
-<meta name="keywords" content="turbofan prognostics, dashboard, predictive maintenance">
-<meta name="description" content="turbofan prognostic dashboard">
-<meta name="author" content="Team prognostics">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<h1 style="font-size:300%; color:#008080; font-family:Georgia"> Turbofan Prognostics <br>
- <h2 style="color:#008080; font-family:Georgia"> DASHBOARD</h3> <br>
- <hr style= "  display: block;
-  margin-top: 0.5em;
-  margin-bottom: 0.5em;
-  margin-left: auto;
-  margin-right: auto;
-  border-style: inset;
-  border-width: 1.5px;"></h1>
-"""
 
-st.set_page_config(page_title="Turbofan Prognostics Dashboard", page_icon="", layout="wide")
-st.markdown('<style>body{background-color: #fbfff0}</style>',unsafe_allow_html=True)
-st.markdown(html_header, unsafe_allow_html=True)
+st.set_page_config(page_title="Turbofan Prognostics Dashboard",
+                   page_icon="", layout="wide")
+st.markdown('<style>body{background-color: #fbfff0}</style>',
+            unsafe_allow_html=True)
+st.markdown(html, unsafe_allow_html=True)
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
-
-# backend request params
-port = 80
-host = 'turbo-fast-app-dev.us-west-2.elasticbeanstalk.com'
-# use this host when testing local FastAPI docker deployment from streamlit
-# host = 'host.docker.internal' 
-url = f'http://{host}:{port}/stream_predict'
+</style> """,
+unsafe_allow_html=True)
 
 st.title('Hello, my name is TurboFan App!')
 st.header('And I like to make engine failure predictions!')
@@ -71,33 +62,46 @@ train_df = load_train_data()
 # get engines
 engines: List[int] = train_df['unit_number'].unique().tolist()
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-engine_select: int = st.sidebar.selectbox(
-                        label="Engine #",
-                        options=engines,
-                        help="Select engine to plot")
+# get models
+models: List[str] = get_models()
 
-# subset training data based on engine selected
-df_ = train_df[train_df['unit_number'] == engine_select]
-df_ = df_.reset_index()
-rul = df_[['RUL']]
-empirical_rul = int(rul.iloc[0])
-df_ = df_.drop(columns=['RUL'])
-placeholder = pd.DataFrame({"empirical_rul": empirical_rul, "predicted_rul": 125}, index=[0])
-st.subheader(f'Emperical and Predicted remaining useful life for engine # {engine_select}')
-chart = st.line_chart(placeholder)
+with st.form(key='simulation_form'):
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    engine_select: int = st.sidebar.selectbox(
+                            label="Select Engine #",
+                            options=engines,
+                            help="Select engine to plot")
+    model_select: str = st.sidebar.selectbox(
+                            label='Select A Model',
+                            options=models,
+                            help='Select model for inference')
+    submit_button = st.form_submit_button(label='Start Simulation')
 
-# run simulation
-data_len = df_.shape[0]
-percent_complete = 0
-for index, row in df_.iterrows():
-    percent_complete = (index + 1) * 1.0 / data_len
-    new_measurement = row.to_dict()
-    response = requests.post(url, json=new_measurement)
-    pred: int = int(response.json()['prediction'])
-    empirical_rul: int = int(rul.iloc[index])
-    status_text.text(f"{percent_complete:.2f} Complete")
-    chart.add_rows(pd.DataFrame({"empirical_rul": empirical_rul, "predicted_rul": pred}, index=[index]))
-    progress_bar.progress(percent_complete)
-    time.sleep(0.05)
+    if submit_button:
+        # subset training data based on engine selected
+        df_ = train_df[train_df['unit_number'] == engine_select]
+        df_ = df_.reset_index()
+        rul = df_[['RUL']]
+        empirical_rul = int(rul.iloc[0])
+        df_ = df_.drop(columns=['RUL'])
+        placeholder = pd.DataFrame({"empirical_rul": empirical_rul,
+                                    "predicted_rul": 125}, index=[0])
+        st.subheader(f'Emperical and Predicted remaining useful life '
+                     f'for engine # {engine_select}')
+        chart = st.line_chart(placeholder)
+
+        # run simulation
+        data_len = df_.shape[0]
+        percent_complete = 0
+        for index, row in df_.iterrows():
+            percent_complete = (index + 1) * 1.0 / data_len
+            new_measurement = row.to_dict()
+            response = requests.post(f'{url}/stream_predict', json=new_measurement)
+            pred: int = int(response.json()['prediction'])
+            empirical_rul: int = int(rul.iloc[index])
+            status_text.text(f"{percent_complete:.2f} Complete")
+            chart.add_rows(pd.DataFrame({"empirical_rul": empirical_rul,
+                                         "predicted_rul": pred}, index=[index]))
+            progress_bar.progress(percent_complete)
+            time.sleep(0.05)
