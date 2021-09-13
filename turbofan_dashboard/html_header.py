863bec0dac1eba998de033f28ec6b28cdd1ef7fc
Abirvab Deb <adeb@concertai.com>
2021-09-13 19:51:48 -0400

    moving html header to separate file

---
+++ turbofan_dashboard/html_header.py
@@ -0,0 +1,19 @@
+html = """
+<head>
+<title>Predictive Maintenance</title>
+<meta charset="utf-8">
+<meta name="keywords" content="turbofan prognostics, dashboard, predictive maintenance">
+<meta name="description" content="turbofan prognostic dashboard">
+<meta name="author" content="Team prognostics">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<h1 style="font-size:300%; color:#008080; font-family:Georgia"> Turbofan Prognostics <br>
+ <h2 style="color:#008080; font-family:Georgia"> DASHBOARD</h3> <br>
+ <hr style= "  display: block;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
+  margin-left: auto;
+  margin-right: auto;
+  border-style: inset;
+  border-width: 1.5px;"></h1>
+"""
