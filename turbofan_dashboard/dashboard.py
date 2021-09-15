4b14b7dd66e0ea752c3f503b9101e49b765dd98f
Abirvab Deb <adeb@concertai.com>
2021-09-15 14:59:12 -0400

    changes for time series model simulation

--- turbofan_dashboard/dashboard.py
+++ turbofan_dashboard/dashboard.py
@@ -12,7 +12,8 @@ import pandas as pd
 import random
 from random import sample
 import json
-from typing import List
+from typing import List, Dict
+import logging
 
 from html_header import html
 
@@ -37,10 +38,26 @@ def load_train_data() -> pd.DataFrame:
     return train_df
 
 @st.cache
-def get_models():
+def get_models() -> List[str]:
     response = requests.get(f'{url}/models')
     return response.json()['models']
 
+def pick_model(model_name: str) -> None:
+    response = requests.post(f'{url}/select_model', json={'model_name': model_name})
+    if response.status_code != 201:
+        raise Exception(f'Model Name {model_name} does not exist!')
+    logging.info('Model loaded!')
+
+def build_payload(df: pd.DataFrame) -> Dict[str, List[float]]:
+    """
+    Generates a JSON payload from a loaded pandas dataframe 
+    """
+    to_return = {}
+    df = df.replace({np.nan: 'NaN'})
+    df_dict: Dict[str, dict] = df.to_dict()
+    for col, inner_dict in df_dict.items():
+        to_return[col] = ["NaN" if val == np.nan else val for index, val in inner_dict.items()]
+    return to_return
 
 st.set_page_config(page_title="Turbofan Prognostics Dashboard",
                    page_icon="", layout="wide")
@@ -91,17 +108,37 @@ with st.form(key='simulation_form'):
                      f'for engine # {engine_select}')
         chart = st.line_chart(placeholder)
 
-        # run simulation
+        # load selected model in backend
+        pick_model(model_select)
+
+        # run simulation (different simulation depending on selected model)
         data_len = df_.shape[0]
         percent_complete = 0
-        for index, row in df_.iterrows():
-            percent_complete = (index + 1) * 1.0 / data_len
-            new_measurement = row.to_dict()
-            response = requests.post(f'{url}/stream_predict', json=new_measurement)
-            pred: int = int(response.json()['prediction'])
-            empirical_rul: int = int(rul.iloc[index])
-            status_text.text(f"{percent_complete:.2f} Complete")
-            chart.add_rows(pd.DataFrame({"empirical_rul": empirical_rul,
-                                         "predicted_rul": pred}, index=[index]))
-            progress_bar.progress(percent_complete)
-            time.sleep(0.05)
+        if model_select == 'Baseline Regression Model':
+            for index, row in df_.iterrows():
+                percent_complete: float = (index + 1) * 1.0 / data_len
+                new_measurement = row.to_dict()
+                response = requests.post(f'{url}/stream_predict', json=new_measurement)
+                pred: int = int(response.json()['prediction'])
+                empirical_rul: int = int(rul.iloc[index])
+                status_text.text(f"{percent_complete:.2f} Complete")
+                chart.add_rows(pd.DataFrame({"empirical_rul": empirical_rul,
+                                             "predicted_rul": pred}, index=[index]))
+                progress_bar.progress(percent_complete)
+                time.sleep(0.05)
+        elif model_select == 'Time Series Regression Model':
+            payload = build_payload(df_)
+            response = requests.post(f'{url}/batch_predict', json=payload)
+            predictions = json.loads(response.json()['predictions'])
+            
+            # run simulation on entire batch of predictions
+            for i, pred in enumerate(predictions):
+                # first index's prediction will be ignored due to NaNs in lag 1 variables
+                index = i + 1
+                percent_complete: float = (index + 1) * 1.0 / data_len
+                empirical_rul: int = int(rul.iloc[index])
+                status_text.text(f"{percent_complete:.2f} Complete")
+                chart.add_rows(pd.DataFrame({"empirical_rul": empirical_rul,
+                                             "predicted_rul": int(pred)}, index=[index]))
+                progress_bar.progress(percent_complete)
+                time.sleep(0.05)
